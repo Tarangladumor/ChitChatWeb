74d2a799959eb325a2d3687151ade7f1a1622127
Tarangladumor <tarangladumor@gmail.com>
2024-07-31 11:20:37 +0530

    final-commit

--- src/app/components/Aavtar.tsx
+++ src/app/components/Aavtar.tsx
@@ -3,6 +3,7 @@
 import { User } from "@prisma/client";
 import Image from "next/image";
 import useActiveList from "../hooks/useActiveList";
+import avatar from '../../../public/Images/placeholder.jpg'
 
 interface AavtarProps {
   user?: User
@@ -16,7 +17,7 @@ const Aavtar: React.FC<AavtarProps> = ({
   return (
     <div className=" relative">
       <div className=" relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
-        <Image alt="Aavtar" src={user?.image || '/images/placeholder.jpg'} fill />
+        <Image alt="Aavtar" src={user?.image || avatar} fill />
       </div>
       {isActive && (
         <span className=" absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
